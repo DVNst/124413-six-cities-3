59e712e4a93a99bf0a72c6b669cd20c18a1bfdbb
DVNst <dvn-studio@mail.ru>
2020-02-15 04:11:31 +0300

    add first snapshots test

---
+++ src/components/main/main.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Main from "./main.jsx";
+
+const COUNT_OFFERS = 312;
+const CITY_PLACES = {
+  placeNames: [`Big room in stylish apartment`, `Beautiful calm room in Amsterdam`, `Comfortable SKY ROOM 12th floor`, `Private Sweet bedroom near Center, near Metro`, `Luxurious room 25 min away from museum district`],
+};
+
+it(`Should App render correctly`, () => {
+  const tree = renderer
+    .create(<Main
+      countOffers={COUNT_OFFERS}
+      cityPlaces={CITY_PLACES}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
