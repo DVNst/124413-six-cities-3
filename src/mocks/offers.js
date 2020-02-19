cb346e55adc2b04cb17a6452a62fabba8b50b282
DVNst <dvn-studio@mail.ru>
2020-02-19 17:19:45 +0300

    add mocks

---
+++ src/mocks/offers.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    placeName: `Beautiful & luxurious apartment at great location`,
+    type: `Apartment`,
+    price: 120,
+    period: `night`,
+    rating: 80,
+    mark: `Premium`,
+    img: `img/apartment-01.jpg`,
+  }, {
+    placeName: `Wood and stone place`,
+    type: `Private room`,
+    price: 80,
+    period: `night`,
+    rating: 80,
+    mark: ``,
+    img: `img/room.jpg`,
+  }, {
+    placeName: `Canal View Prinsengracht`,
+    type: `Apartment`,
+    price: 132,
+    period: `night`,
+    rating: 80,
+    mark: ``,
+    img: `img/apartment-02.jpg`,
+  }, {
+    placeName: `Nice, cozy, warm big bed apartment`,
+    type: `Apartment`,
+    price: 180,
+    period: `night`,
+    rating: 100,
+    mark: `Premium`,
+    img: `img/apartment-03.jpg`,
+  },
+];
